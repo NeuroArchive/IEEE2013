9945137f00fb2a5670f177051267f2e630b296a4
Dr Romain Caze <rcaze@imperial.ac.uk>
2013-10-15 12:37:26 +0100

    Version used to generate the results pres in conf

--- func.py
+++ func.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from traits.api import HasTraits, Int, Array, Bool
 from itertools import combinations_with_replacement as comb
 from itertools import product as prod
+from itertools import permutations
 import re
 
 
@@ -88,6 +89,46 @@ def select_functions(selection):
     return [i for i, test in enumerate(selection) if test]
 
 
+def gperms(n):
+    """Generate all the possible permutations of the input vectors you can obtain
+    by permuting the label of the input lines.
+
+    Parameters
+    ----------
+    n : an integer
+        number of input variables
+
+    Returns
+    -------
+    perms : a list of integer lists
+        All possible permutations of the input vector set when labels are permuted.
+
+    Examples
+    --------
+    >>> gperms(n=2)
+    [[0,1,2,3],[0,2,1,3]]
+
+    Comments
+    --------
+    The first list is the non-permuted input vector set.
+    The number of lists is equal to n factorial, a vectorized form would be faster
+    but it is okay for n=7.
+
+    """
+    ipt = [list(iptc) for iptc in iptgen(n)]
+    permipt = [0 for i in range(len(ipt))]
+    perms = list()
+
+    for per in permutations(range(len(ipt[0]))):
+        #Recontruct a new permuted input space
+        permipt = [[ic[i] for i in per] for ic in ipt]
+        #Recording this permuted input space into a list of list
+        perms.append([int(''.join([str(i) for i in ic]),base=2) for ic in permipt])
+
+    return np.array(perms, dtype=np.int)
+
+
+
 class PossibleSetTlu(HasTraits):
     """Wrapper of the different functions"""
     n = Int(4)
@@ -97,32 +138,53 @@ class PossibleSetTlu(HasTraits):
     t_max = Int(5)
     sparsity = Int(-1)
     order = Bool(True)
+    verbose = Bool(False)
     parameters = Array(dtype=np.int)
     functions = Array(dtype=np.int)
-    ipt = Array(dtype=np.int)
-
-    def __init__(self):
-        """Set default parameter value"""
-        self.ipt = iptgen(self.n)
-        self.integration_result()
-        self.threshold_integration()
-        self.generating_fbp()
-        self.analysis()
+    sub_parameters = Array(dtype=np.int)
+    sub_functions = Array(dtype=np.int)
+    ipt = Array(value=iptgen(4), dtype=np.int)
 
     def _anytrait_changed(self, name, old, new):
-        """ """
-        if name == "sparsity":
+        """Deal with changes in trait"""
+        #print name
+        if name == "n":
+            self.ipt = iptgen(new)
             self.integration_result()
             self.threshold_integration()
-            self.select()
             self.generating_fbp()
             self.analysis()
-            #from pdb import set_trace; set_trace()
+
+        if name in ["w_min", "w_max"]:
+            self.integration_result()
+            self.threshold_integration()
+            self.analysis()
+
+        if name in ["t_min", "t_max"]:
+            self.threshold_integration()
+            self.analysis()
+
+        if name == "order":
+            self.integration_result()
+            self.threshold_integration()
+            self.generating_fbp()
+            self.analysis()
+
+        if name == "sparsity":
+            #Select the precise subset of vector
+            self.select()
+            #Recompute the fbp for this subset
+            self.generating_fbp()
+            if new == self.n/2:
+                #Recalculate the analysis
+                self.analysis()
 
     def integration_result(self):
         """Generate the list of all possible integration result
         and the associated parameter set
         """
+        if self.verbose:
+            print "Computing synaptic integration \n"
         ipt = np.rot90(self.ipt)
         w_min = self.w_min
         w_max = self.w_max
@@ -142,6 +204,8 @@ class PossibleSetTlu(HasTraits):
 
     def threshold_integration(self):
         """Threshold the result of an integration and output the result"""
+        if self.verbose:
+            print "Thresholding integration \n"
         integration = self.integration
         weights = self.weights
         t_min = self.t_min
@@ -167,6 +231,8 @@ class PossibleSetTlu(HasTraits):
     def generating_fbp(self):
         """generate the fbp function whatever the size of
         the vector to be classified"""
+        if self.verbose:
+            print "Generate the FBP \n"
         n = self.n
         s = self.sparsity
         exp0 = r"[0-1]{%d}1{%d}" % (n/2, n/2)
@@ -185,7 +251,12 @@ class PossibleSetTlu(HasTraits):
 
     def analysis(self):
         couples = []
-        for i in self.functions:
+        if self.sparsity >= 0:
+            f_set = self.sub_functions
+        else:
+            f_set = self.functions
+
+        for i in f_set:
             couples.append(signal_theory_analysis(self.fbp, i))
         self.couples = np.unique(couples)
 
@@ -200,7 +271,7 @@ class PossibleSetTlu(HasTraits):
         selection = self.select_subset()
         par = self.parameters
         sel = [i for i, test in enumerate(selection) if test]
-        self.functions, self.parameters = uniquify_function(self.functions[:, sel],
+        self.sub_functions, self.sub_parameters = uniquify_function(self.functions[:, sel],
                                                             par)
 
     def show(self):
